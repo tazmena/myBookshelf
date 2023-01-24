0304f8524fad5a0fb00c7274074f46f02d5f8860
tazmena <78047816+tazmena@users.noreply.github.com>
2023-01-24 01:38:01 +0000

    User model created

--- myBookshelf/myBookshelf/settings.py
+++ myBookshelf/myBookshelf/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bookapp',
 ]
 
 MIDDLEWARE = [
@@ -116,6 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+AUTH_USER_MODEL = 'bookapp.User'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
