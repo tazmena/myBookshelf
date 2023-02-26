7e666cb9f9f1715ed3e89bf4a3c226ef9d060bd8
tazmena <78047816+tazmena@users.noreply.github.com>
2023-02-26 23:45:41 +0000

    Login Signup Done

--- myBookshelf/bookapp/models.py
+++ myBookshelf/bookapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    email = models.EmailField(max_length=250,unique=True)
+    username = models.CharField(max_length=150,unique=True)
+    password = models.CharField(max_length=250)
+    first_name = models.CharField(max_length=250)
+    last_name = models.CharField(max_length=250)
+    
+
+    def __str__(self):
+        return self.username
